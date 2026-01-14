79d1cf72e90ee658c29921ab7fa7eeaea273ada6
oEdneiAresco <72020263+oEdneiAresco@users.noreply.github.com>
2026-01-14 16:45:18 -0300

    Novo forumlário - carregar planilhas
    Formulário para carregar planilhas

---
+++ Aldeia.Mercosul.Planilhas/1. UI/frmCarregarPlanilhas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aldeia.Mercosul.Planilhas._1._UI
+{
+    public partial class frmCarregarPlanilhas : Form
+    {
+        public frmCarregarPlanilhas()
+        {
+            InitializeComponent();
+        }
+    }
+}
